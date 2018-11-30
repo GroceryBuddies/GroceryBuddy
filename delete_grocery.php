3c1d7feb90378266edf4eb697c288473bb91d217
Josh Gordon <jcg525@msstate.edu>
2018-11-29 22:40:46 -0600

    Updated the methods to delete everything needed from the db.

--- delete_grocery.php
+++ delete_grocery.php
@@ -3,6 +3,14 @@ require_once 'login.php';
     $connection = new mysqli($hn, $un, $pw, $db);
     if ($connection->connect_error) die($connection->connect_error);
     $groceryName = $_POST['id'];
-    $query = "DELETE FROM gb_Groceries WHERE itemName=$groceryName";
+    $query = "SELECT * FROM gb_Groceries WHERE itemName=$groceryName";
+    $result = $connection->query($query);
+    $row = $result->fetch_array();
+    $itemID = $row[itemID];
+    $query = "DELETE FROM gb_Join WHERE itemID=$itemID";
+    $result = $connection->query($query);
+    $query = "DELETE FROM gb_ShoppingList WHERE itemID=$itemID";
+    $result = $connection->query($query);
+    $query = "DELETE FROM gb_Groceries WHERE itemID=$itemID";
     $result = $connection->query($query);
 ?>
--- delete_user.php
+++ delete_user.php
@@ -3,6 +3,21 @@ require_once 'login.php';
     $connection = new mysqli($hn, $un, $pw, $db);
     if ($connection->connect_error) die($connection->connect_error);
     $username = $_POST['id'];
-    $query = "DELETE FROM gb_Users WHERE username=$username";
+    $query = "SELECT * FROM gb_Users WHERE username=$username";
     $result = $connection->query($query);
+    $row = $result->fetch_array();
+    $userID = $row[userID];
+    $query = "DELETE FROM gb_ShoppingList WHERE userID=$userID";
+    $connection->query($query);
+    $query = "SELECT * FROM gb_Recipes WHERE userID=$userID";
+    $recipeResult = $connection->query($query);
+    while($row = $recipeResult->fetch_array()){
+        $recipeID = $row[recipeID];
+        $query = "DELETE FROM gb_Join WHERE recipeID=$recipeID";
+        $connection->query($query);
+    }
+    $query = "DELETE FROM gb_Recipes WHERE userID=$userID";
+    $connection->query($query);
+    $query = "DELETE FROM gb_Users WHERE userID=$userID";
+    $connection->query($query);
 ?>
