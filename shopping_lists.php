3a2871e96fca82f4f084716ac0aae85fc1339c66
Josh Gordon <jcg525@msstate.edu>
2018-12-05 15:42:39 -0600

    Removed the add buttons that were unnecesary.

--- shopping_lists.php
+++ shopping_lists.php
@@ -24,49 +24,7 @@
 <body>
 <h1>
     Shopping List
-    <button id="add" type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModalCenter">
-    <b>Add</b>
-    </button>
-    </h1>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Add Items to Grocery List</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <!-- Add HTML/JS/PHP here. -->
-            <form>
-                <div class="form-group">
-                <label for="message-text" class="col-form-label">Quantity</label>
-                    <input class="form-control" id="quantity"></textarea>
-                    <label for="message-text" class="col-form-label">Grocery</label>
-                    <input class="form-control" id="name"></textarea>
-                </div>
-            </form>
-            <!-- END -->
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-        </div>
-        </div>
-        </div>
-    </div>
-    </div>
-
-    <script>
-        $('#myModal').on('shown.bs.modal', function () {
-            $('#myInput').trigger('focus')
-            })
-    </script>
-
+</h1>
     <br>
     <table class="table">
         <thead>
